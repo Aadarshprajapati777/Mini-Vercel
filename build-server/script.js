2a8ee04ce509ec5aeebb1638ee8b57b91e1c5e7a
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-02-10 22:27:43 +0530

    commands sent to s3client

--- build-server/script.js
+++ build-server/script.js
@@ -1,5 +1,21 @@
 const {exec} = require('child_process')
 const path = require('path')
+const fs= require('fs')
+
+const {S3Client, PutObjectCommand} =require('@aws-sdk/client-s3')
+const mime = require('mime-types')
+
+
+const s3Client = new S3Client({
+    region: '',
+    credentials:{
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+})
+
+
+const PROJECT_ID = process.env.PROJECT_ID;
 
 async function init(){
     console.log('Executing script.js')
@@ -14,7 +30,22 @@ async function init(){
         console.log('Error', data.toString())
     })
 
-    p.on('close', function(){
+    p.on('close', async function(){
         console.log('build complete')
+        const distFolderPath= path.join(__dirname, 'output', 'dist')
+        const distFolderContents = fs.readdirSync(distFolderPath,{recursive: true})
+        for (const filePath of distFolderContents){
+            if(fs.lstatSync(filePath).isDirectory()) continue;
+            const comand = new PutObjectCommand({
+                Bucket: '', 
+                Key: `__outputs/${PROJECT_ID}/${filePath}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+            })
+
+            await s3Client.send(comand)
+        }
+
+        console.log('Done...............')
     })
 }
